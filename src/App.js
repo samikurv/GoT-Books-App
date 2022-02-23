100d059d371fc4aeaaa4b28638add3f862d5d909
Sami Kurvinen <n0kusa00@students.oamk.fi>
2022-02-23 15:46:04 +0200

    helveTTI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+
+  const [books, setBooks] = useState(null);
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://www.anapioficeandfire.com/api/books?pageSize=30"
+    );
+
+    setBooks(response.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Game of Thrones Books</h1>
+      <br />
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button" onClick={fetchData}>
+          All Books
+        </button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+      <div className="books">
+        {books &&
+          books.map((book, index) => {
+            const cleanedDate = new Date(book.released).toDateString();
+            const authors = book.authors.join(", ");
+
+            return (
+              <div className="book" key={index}>
+                <h3>Book {index + 1}</h3>
+                <h2>{book.name}</h2>
+
+                <div className="details">
+                  <p>{authors}</p>
+                  <p>{book.numberOfPages} pages</p>
+                  <p>🏘{book.country}</p>
+                  <p>{cleanedDate}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
+
+  
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
